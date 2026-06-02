2d54c0238273dba50a2d7a2f8287766a3c5ff135
Payal Chaudhary <chpayal747@gmail.com>
2026-06-02 17:36:12 +0530

    Day-3 uploaded.

---
+++ Day-3/Q9.cpp
@@ -0,0 +1,29 @@
+//Write a program to Check whether a number is prime.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, i;
+    bool isPrime = true;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n <= 1) {
+        isPrime = false;
+    } else {
+        for (i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        cout << n << " is a Prime Number.";
+    else
+        cout << n << " is not a Prime Number.";
+
+    return 0;
+}
